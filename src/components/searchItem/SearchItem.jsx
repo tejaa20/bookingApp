9c1e88d16e5cad035bd0b6da0194f3502ed60ccf
Varupula Tejaswini <tejaswinivarupula2006@gmail.com>
2025-06-07 11:51:39 +0530

    Update SearchItem.jsx

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -1,6 +1,11 @@
 import "./searchItem.css";
+import { useNavigate } from "react-router-dom";
 
 const SearchItem = () => {
+  const navigate = useNavigate();
+    const handleSearch = () => {
+    navigate("/hotels/id");
+  };
   return (
     <div className="searchItem">
       <img
@@ -31,7 +36,7 @@ const SearchItem = () => {
         <div className="siDetailTexts">
           <span className="siPrice">$112</span>
           <span className="siTaxOp">Includes taxes and fees</span>
-          <button className="siCheckButton">See availability</button>
+          <button className="siCheckButton" onClick={handleSearch}>See availability</button>
         </div>
       </div>
     </div>
